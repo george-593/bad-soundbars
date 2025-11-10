1e65358b8ffaf73ee508bddf7095f68cc4f0cd89
george-593 <63100333+george-593@users.noreply.github.com>
2025-11-10 14:14:46 +0000

    Terms and conditions page

--- bureaucratic-hell/js/bh.js
+++ bureaucratic-hell/js/bh.js
@@ -22,8 +22,22 @@ modalCancel.addEventListener('click', closeModal);
 modalConfirm.addEventListener('click', openTnC);
 
 // Terms and conditions 
-tncWindow = document.getElementById("tandc-window")
-
 function openTnC() {
-    tncWindow.classList.remove("hidden")
+    const tncWindow = document.getElementById("tandc-window");
+    const scrollText = document.getElementById("tandc-scroll-text");
+    const tncButton = document.getElementById("tandc-btn");
+    const tncCheckBox = document.getElementById("tandc-checkbox")
+
+    if (tncWindow) tncWindow.classList.remove("hidden");
+
+    const tolerance = 5;
+
+    const handler = function () {
+        if (scrollText.scrollTop + scrollText.clientHeight >= scrollText.scrollHeight - tolerance) {
+            tncCheckBox.disabled = false
+            tncButton.disabled = false;
+            scrollText.removeEventListener('scroll', handler);
+        }
+    };
+    scrollText.addEventListener('scroll', handler);
 }
